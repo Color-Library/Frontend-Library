219375db282934253304ec2ca193bf24cddc322c
dreayoung <youngmandrea@gmail.com>
2021-07-22 14:22:00 -0400

    navbar cleanup

--- color_scheme_app/src/components/Header.js
+++ color_scheme_app/src/components/Header.js
@@ -5,19 +5,19 @@ import "../styles/Header.css"
 export default function Header ({ tab1, tab2, tab3, tab4, backgroundColor, }) {
     return (
         <div className="topnav" style={backgroundColor && { backgroundColor }}>
-          <a className="leftnav" href="#">{tab1}</a>
-          <a className="leftnav" href="#">{tab2}</a>
-          <a className="rightnav" href="#">{tab3}</a>
-          <a className="rightnav" href="#">{tab4}</a>
+          <a className="leftnav" href={tab1[1]}>{tab1[0]}</a>
+          <a className="leftnav" href={tab2[1]}>{tab2[0]}</a>
+          <a className="rightnav" href={tab3[1]}>{tab3[0]}</a>
+          <a className="rightnav" href={tab4[1]}>{tab4[0]}</a>
         </div>
     )
 }
 
 Header.propTypes = {
-    tab1: PropTypes.string.isRequired,
-    tab2: PropTypes.string.isRequired,
-    tab3: PropTypes.string.isRequired,
-    tab4: PropTypes.string.isRequired,
+    tab1: PropTypes.arrayOf(PropTypes.string).isRequired,
+    tab2: PropTypes.arrayOf(PropTypes.string).isRequired,
+    tab3: PropTypes.arrayOf(PropTypes.string).isRequired,
+    tab4: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 Header.defaultProps = {};
--- color_scheme_app/src/stories/Header.stories.js
+++ color_scheme_app/src/stories/Header.stories.js
@@ -13,19 +13,28 @@ export default {
 
   export const frozen2 = Template.bind({});
   frozen2.args = {
-    tab1: 'Home',
-    tab2: 'About',
-    tab3: '',
-    tab4: '',
+    tab1: ['Home', '#'],
+    tab2: ['About', '#'],
+    tab3: ['', ''],
+    tab4: ['Profile', '#'],
     backgroundColor: '#084C91',
   }
 
   export const frozen1 = Template.bind({});
   frozen1.args = {
-    tab1: 'Home',
-    tab2: 'About',
-    tab3: '',
-    tab4: '',
+    tab1: ['Home', '#'],
+    tab2: ['About', '#'],
+    tab3: ['', ''],
+    tab4: ['', ''],
     backgroundColor: '#97d8ea',
+  }
+
+  export const pastels1 = Template.bind({});
+  pastels1.args = {
+    tab1: ['Home', '#'],
+    tab2: ['About', '#'],
+    tab3: ['', ''],
+    tab4: ['', ''],
+    backgroundColor: '#dfd5fc',
   }
 

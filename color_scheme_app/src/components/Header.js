641ca7e4f90b86bfeec42b76f0a52aff2d0b2a05
dreayoung <youngmandrea@gmail.com>
2021-07-23 09:18:55 -0400

    navbar logogs

--- color_scheme_app/src/components/Header.js
+++ color_scheme_app/src/components/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "../styles/Header.css"
 
-export default function Header ({ tab1, tab2, tab3, tab4, backgroundColor, }) {
+export default function Header ({ tab1, tab2, tab3, tab4, backgroundColor, color, logo }) {
     return (
         <div className="topnav" style={backgroundColor && { backgroundColor }}>
-          <a className="leftnav" href={tab1[1]}>{tab1[0]}</a>
-          <a className="leftnav" href={tab2[1]}>{tab2[0]}</a>
-          <a className="rightnav" href={tab3[1]}>{tab3[0]}</a>
-          <a className="rightnav" href={tab4[1]}>{tab4[0]}</a>
+          <img src={logo} className="iconlogo"/>
+          <a className="leftnav" style={color && { color }} href={tab1[1]}>{tab1[0]}</a>
+          <a className="leftnav" style={color && { color }} href={tab2[1]}>{tab2[0]}</a>
+          <a className="rightnav" style={color && { color }} href={tab3[1]}>{tab3[0]}</a>
+          <a className="rightnav" style={color && { color }} href={tab4[1]}>{tab4[0]}</a>
         </div>
     )
 }
--- color_scheme_app/src/stories/Header.stories.js
+++ color_scheme_app/src/stories/Header.stories.js
@@ -6,6 +6,7 @@ export default {
     component: Header,
     argTypes: {
         backgroundColor: { control: 'color' },
+        color: { control: 'color' },
       },
   };
 
@@ -18,23 +19,27 @@ export default {
     tab3: ['', ''],
     tab4: ['Profile', '#'],
     backgroundColor: '#084C91',
+    logo: "https://img.icons8.com/ios/50/000000/pokemon.png",
   }
 
   export const frozen1 = Template.bind({});
   frozen1.args = {
     tab1: ['Home', '#'],
     tab2: ['About', '#'],
-    tab3: ['', ''],
-    tab4: ['', ''],
+    tab3: ['Profile', '#'],
+    tab4: ['Contact', '#'],
     backgroundColor: '#97d8ea',
+    logo: "https://img.icons8.com/office/50/000000/blackblood.png",
   }
 
   export const pastels1 = Template.bind({});
   pastels1.args = {
     tab1: ['Home', '#'],
     tab2: ['About', '#'],
-    tab3: ['', ''],
-    tab4: ['', ''],
+    tab3: ['Profile', '#'],
+    tab4: ['Contact', '#'],
     backgroundColor: '#dfd5fc',
+    color: 'black',
+    logo: "https://img.icons8.com/office/50/000000/jake.png",
   }
 

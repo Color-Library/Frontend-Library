5653e1ccc1b128c6900eb3057887827fffed1b68
dreayoung <youngmandrea@gmail.com>
2021-07-25 14:55:06 -0400

    add ons

--- color_scheme_app/src/stories/Header.stories.js
+++ color_scheme_app/src/stories/Header.stories.js
@@ -22,24 +22,33 @@ export default {
     logo: "https://img.icons8.com/ios/50/000000/pokemon.png",
   }
 
-  export const frozen1 = Template.bind({});
-  frozen1.args = {
+  export const sunflower1 = Template.bind({});
+  sunflower1.args = {
     tab1: ['Home', '#'],
     tab2: ['About', '#'],
     tab3: ['Profile', '#'],
     tab4: ['Contact', '#'],
-    backgroundColor: '#97d8ea',
+    backgroundColor: '#E8C78E',
     logo: "https://img.icons8.com/office/50/000000/blackblood.png",
   }
 
-  export const pastels1 = Template.bind({});
-  pastels1.args = {
+  export const perwinkles1 = Template.bind({});
+  perwinkles1.args = {
     tab1: ['Home', '#'],
     tab2: ['About', '#'],
     tab3: ['Profile', '#'],
     tab4: ['Contact', '#'],
-    backgroundColor: '#dfd5fc',
+    backgroundColor: '#E5D9F2',
     color: 'black',
     logo: "https://img.icons8.com/office/50/000000/jake.png",
   }
 
+export const spring1 = Template.bind({});
+spring1.args = {
+  tab1: ['Home', '#'],
+    tab2: ['About', '#'],
+    tab3: ['Profile', '#'],
+    tab4: ['Contact', '#'],
+    backgroundColor: '9BE564',
+    logo: "https://img.icons8.com/nolan/50/iron-man.png",
+}
